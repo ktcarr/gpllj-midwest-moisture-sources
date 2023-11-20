c6257f41f9abf305e90e393865fd93d988d20a04
Theo Carr <ktcarr@mit.edu>
2023-11-20 11:52:30 -0500

    save figs as 'svg' instead of 'png'

--- src/utils.py
+++ src/utils.py
@@ -101,7 +101,7 @@ def save(fig, fname, is_supp=True):
     # about intersections.
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        fig.savefig(f"{save_fp}/{fname}.png")
+        fig.savefig(f"{save_fp}/{fname}.svg", format="svg")
     return
 
 
